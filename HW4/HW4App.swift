23b2cf251667e0306894a690c6e306a055a9aaf7
armanjakhayev <67386397+armanjakhayev@users.noreply.github.com>
2021-12-07 16:17:02 -0800

    Initial Commit

---
+++ HW4/HW4App.swift
@@ -0,0 +1,17 @@
+//
+//  HW4App.swift
+//  HW4
+//
+//  Created by Arman on 12/7/21.
+//
+
+import SwiftUI
+
+@main
+struct HW4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
